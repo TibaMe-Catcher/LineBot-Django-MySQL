6e6898bd2462ba13b07d7cc0cdd763b867c9bdfa
coco <alviss4582@gmail.com>
2020-11-22 23:56:17 +0800

    增加顯示表格的模板

--- proj_catcher/app_catcher/admin.py
+++ proj_catcher/app_catcher/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app_catcher.models import *    # 把 user 資料表加入顯示的表單
 
-# Register your models here.
+class usersAdmin(admin.ModelAdmin):     # 將 uid 和 question 列為顯示的欄位
+    list_display = ('uid', 'datatest')
+admin.site.register(users, usersAdmin)
+
+'''
+class diaryAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'diaryid', 'content', 'created_at')
+admin.site.register(diary, diaryAdmin)
+'''
--- proj_catcher/app_catcher/models.py
+++ proj_catcher/app_catcher/models.py
@@ -3,15 +3,28 @@ from django.db import models
 # Line ID 資料收集
 class users(models.Model):
     uid = models.CharField(max_length=50, null=False)      # uid 欄位儲存使用者的 Line ID
-    datatest = models.CharField(max_length=50, null=False)
+    datatest = models.CharField(max_length=300, null=False)
     
     def __str__(self):
         return self.uid
 
+# 使用者 Diary 紀錄
+class diary(models.Model):
+    uid = models.CharField(max_length=50, null=False)
+    diaryid = models.CharField(max_length=50, null=False)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# 使用者 PHQ9紀錄
+    def __str__(self):
+        return self.uid
+
+# 
+
+
+
+# 使用者 PHQ9 紀錄
 class phq9(models.Model):
-    phq_id = models.CharField(max_length=50, default='0', null=False)
+    uid = models.CharField(max_length=50, default='0', null=False)
     num01 = models.CharField(max_length=1, default='0', null=False)
     num02 = models.CharField(max_length=1, default='0', null=False)
     num03 = models.CharField(max_length=1, default='0', null=False)
@@ -21,6 +34,8 @@ class phq9(models.Model):
     num07 = models.CharField(max_length=1, default='0', null=False)
     num08 = models.CharField(max_length=1, default='0', null=False)
     num09 = models.CharField(max_length=1, default='0', null=False)
+    phqall = models.CharField(max_length=2, default='0', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.phq_id
+        return self.uid
