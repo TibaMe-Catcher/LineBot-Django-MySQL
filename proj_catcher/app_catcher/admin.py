b337cb95b3b2b1c065a43bef33d3478f35f02f84
coco <alviss4582@gmail.com>
2020-12-12 03:46:42 +0800

    增加一個Surprise的表單，做為小小驚喜的推薦內容

--- proj_catcher/app_catcher/admin.py
+++ proj_catcher/app_catcher/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from app_catcher.models import *    # 把 user 資料表加入顯示的表單
 
-class UsersAdmin(admin.ModelAdmin):     # 將 uid 和 question 列為顯示的欄位
-    list_display = ('uid', 'created_at', 'phq_score',)
-admin.site.register(Users, UsersAdmin)
+# class UsersAdmin(admin.ModelAdmin):     # 將 uid 和 question 列為顯示的欄位
+#     list_display = ('uid', 'created_at', 'phq_score',)
+# admin.site.register(Users, UsersAdmin)
 
 
 class DiaryAdmin(admin.ModelAdmin):
@@ -16,6 +16,6 @@ class PictureAdmin(admin.ModelAdmin):
 admin.site.register(Picture, PictureAdmin)
 
 
-# class QuestionAdmin(admin.ModelAdmin):
-#     list_display = ('question_id', 'self_mood', 'self_sleep', 'self_sleep_status', 'self_eat', 'self_energy', 'created_at', 'uid',)
-# admin.site.register(Question, QuestionAdmin)
+class SurpriseAdmin(admin.ModelAdmin):
+    list_display = ('sur_id', 'category', 'mood', 'title', 'content')
+admin.site.register(Surprise, SurpriseAdmin)
--- proj_catcher/app_catcher/models.py
+++ proj_catcher/app_catcher/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-#Line ID 資料收集
-class Users(models.Model):
-    uid = models.CharField(max_length=50, null=False)      # uid 欄位儲存使用者的 Line ID
-    created_at = models.DateTimeField(auto_now_add=True)
-    phq_score = models.CharField(max_length=50, null=False)
+# Line ID 資料收集
+# class Users(models.Model):
+#     uid = models.CharField(max_length=50, null=False)      # uid 欄位儲存使用者的 Line ID
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     phq_score = models.CharField(max_length=50, null=False)
     
-    def __str__(self):
-        return self.uid
+#     def __str__(self):
+#         return self.uid
 
 
-#使用者 Diary 紀錄
+# 使用者 Diary 紀錄
 class Diary(models.Model):
     diary_id = models.CharField(blank=False, max_length=15, default=None)
     text = models.TextField(blank=True, default=None)
@@ -33,18 +33,17 @@ class Picture(models.Model):
         return self.pirture_id
 
 
-# # 使用者每日主觀評估
-# class Question(models.Model):
-#     question_id = models.CharField(max_length=50, null=False, default=None)
-#     self_mood = models.IntegerField(default='0', null=False)          # 給自己昨天的心情幾分?
-#     self_sleep_status = models.IntegerField(default='0', null=False)  # 昨晚睡眠狀態如何?
-#     self_eat = models.IntegerField(default='0', null=False)           # 昨天用餐 / 胃口狀態如何?
-#     self_energy = models.IntegerField(default='0', null=False)        # 昨天體力狀況?
-#     created_at = models.DateTimeField(auto_now_add=True)              # 填寫問卷時間
-#     uid = models.CharField(max_length=50, null=False)                 # 使用者是誰?
 
-#     def __str__(self):
-#         return self.question_id
+# 使用者 Surprise 推薦
+class Surprise(models.Model):
+    sur_id = models.CharField(blank=False, max_length=15, default=None)
+    category = models.TextField(blank=True, default=None)
+    mood = models.TextField(blank=True, default=None)
+    title = models.TextField(blank=True, default=None)
+    content = models.TextField(blank=True, max_length=300, default=None)
+
+    def __str__(self):
+        return self.sur_id
 
 
 
