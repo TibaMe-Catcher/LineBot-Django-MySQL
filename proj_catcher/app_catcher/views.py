eb8c12be4a089fc04317d52d3e9d92f55c4c781a
coco <alviss4582@gmail.com>
2020-12-12 22:07:11 +0800

    完成了~~~有些小地方以後再改~~XDD

--- proj_catcher/app_catcher/views.py
+++ proj_catcher/app_catcher/views.py
@@ -14,7 +14,7 @@ from linebot.models.template import *
 import os, tempfile, datetime, errno, json, sys    # 常用的模組
 from PIL import Image, ImageDraw                   # 圖片及臉部辨識模組
 from io import BytesIO
-
+import pandas as pd
 
 from .models import *
 from module import func
@@ -59,12 +59,16 @@ def handle_text_message(event):
     elif text == '@心情問卷@':      # 點擊圖文選單，出現@心情問卷@     → 回傳google問卷(想下要怎麼顯示，不想URL)
         func.sendText4(event)
     elif text == '@心情回顧@':      # 點擊圖文選單，出現@心情回顧@     → 顯示一周心情波動圖表   (可能出現心情天氣icon)
-        pass
+        func.sendText6(event)
     elif text == '@小小驚喜@':      # 點擊圖文選單，出現@小小驚喜@     → 回傳好聽的音樂或是美食資訊
-        pass
+        func.sendText5(event)
+    elif text == '_今天_':          # 點擊選單，出現_今天_
+        func.sendText7(event)
+    elif text == '_一周_':          # 點擊選單，出現_一周_
+        func.sendText8(event)
     else:                           # 假若不是預設功能的文字，就將文字訊息存入SQL
         message_id = event.message.id
-        text = event.message.text
+        text = [event.message.text]
         diary_uid = event.source.user_id
 
         # message = [TextSendMessage(text = "text：" + text + "\n" + 
@@ -76,7 +80,33 @@ def handle_text_message(event):
         #     myfile.write(json.dumps(text,sort_keys=True))
         #     myfile.write('\r\n')
         
-        unit = Diary.objects.create(diary_id = message_id, text = text, diary_uid=diary_uid)
+        # 呼叫模型比對文字是否有憂鬱，1是憂鬱，2是健康
+        text_score = int(func.svm_test(text))
+
+        # 回應使用者的對話
+        if text == ["沒有"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的，已經幫你紀錄，可以點擊"心情回顧"追蹤心情天氣喔~'))
+        elif text == ["沒有啦"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的，請點擊"心情回顧"，來追蹤自己的心情天氣!!'))
+        elif text == ["沒有了"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='OK，已經幫你紀錄，請點擊"心情回顧"追蹤心情天氣~'))
+        elif text == ["無"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的，已經幫你紀錄，可以點擊"心情回顧"追蹤心情天氣喔~'))
+        elif text == ["no"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='OK，已經幫你紀錄，可以點擊"心情回顧"追蹤心情天氣喔~'))        
+        elif text == ["0"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好勒，已經幫你紀錄，可以點擊"心情回顧"追蹤心情天氣喔~'))
+        elif text == ["沒了"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='OK，請點擊"心情回顧"追蹤你的心情天氣~'))         
+        elif text == ["冇"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的，快點擊"心情回顧"追蹤心情天氣喔~'))                 
+        elif text == ["沒"]:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='OK，已經幫你紀錄，可以點擊"心情回顧"追蹤心情天氣喔~'))                
+        else :
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的，還有什麼可以跟我分享的嗎?可以再多說一點'))
+
+        # 存進資料庫
+        unit = Diary.objects.create(diary_id = message_id, text = text, diary_uid=diary_uid, text_score=text_score)
 
 
 @handler.add(MessageEvent, message=ImageMessage)    # 若訊息是圖片，則執行以下的操作
@@ -95,7 +125,7 @@ def handle_image_message(event):
     # line_bot_api.reply_message(event.reply_token, message)
     
     # 呼叫模型比對照片的情緒
-    picture_score = int(func.cv_test(filename))
+    picture_score = func.cv_test(filename)
 
     if picture_score == 0:    # 假如判斷是 "angry"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='什麼事情惹你不高興?來跟我說說吧'))
@@ -108,7 +138,7 @@ def handle_image_message(event):
     elif picture_score == 4:  # 假如判斷是 "surprise"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='什麼事情讓你這麼吃驚?!'))
 
-
+    # 存進資料庫
     unit = Picture.objects.create(picture_id=picture_id, picture_score=picture_score, picture_uid=picture_uid)
     
 
--- proj_catcher/module/func.py
+++ proj_catcher/module/func.py
@@ -11,6 +11,13 @@ import dlib
 import math,  cv2, glob, joblib
 import numpy as np
 from sklearn.svm import SVC
+import pandas as pd
+
+# 分析文字需要的套件
+import jieba
+import joblib
+from sklearn.svm import SVC
+
 
 # google表單存進MySQL的function，之後要整併成一個大的function，等view那邊寫好後這邊整合
 import pymysql
@@ -39,13 +46,13 @@ def sendText2(event):
     try:
         message = [
             TextSendMessage(  
-            text = "Hola ~~\n我是Catcher，很高興認識你~\n\n" +
-                   "下方菜單欄中有需多小功能，有時間都可以嘗試看看喔~\n\n" +
-                   " '我的頭像'：拍個自拍照，讓我瞧瞧現在的你\n" +
-                   " '心情日記'：寫下今天的日記或是趣事雜記吧\n" +
-                   " '健康檢測'：做個心理健康評估\n" +
-                   " '心情回顧'：回顧一周的心情天氣\n" + 
-                   " '小小驚喜'：點擊看看就知道了 >u<\n")
+            text = "Hola ~~\n" +
+                   "下方菜單欄中有需多小功能，\n有時間都可以嘗試看看喔~\n\n" +
+                   " 每日一照：拍張照片，讓我瞧瞧現在的你\n" +
+                   " 心情日記：跟我分享今天發生哪些事情?\n" +
+                   " 心情問卷：做個心理健康評估\n" +
+                   " 心情回顧：回顧一周的心情天氣\n" + 
+                   " 小小驚喜：點擊看看就知道了 >v<\n")
             ]
         line_bot_api.reply_message(event.reply_token,message)
     except:
@@ -74,10 +81,199 @@ def sendText4(event):
 
 #=======================================================================================================
 
+def sendText5(event):
+    '''@小小驚喜@'''
+    # 記得要先從models.py import 小驚喜的class
 
+    try:
+        # 資料庫設定
+        db_settings = {
+            'host': '127.0.0.1',
+            'port': 3306,
+            'user': 'root',
+            'password': 'abc123',
+            'db': 'line_catcher',
+            'charset': 'utf8'
+        }
 
+        connection = pymysql.connect(**db_settings)    # 建立 connection 物件
+        
+        with connection.cursor() as cursor:
+            surp =("SELECT * FROM line_catcher.app_catcher_surprise ORDER BY RAND() LIMIT 1")
+            cursor.execute(surp)
+        connection.commit()
 
+        result = cursor.fetchall()[0]                  # result的類型是tuple
 
+        if result[2] == "music":
+            text = "想跟你分享一首好聽的歌！ \n[{}]\n{}".format(result[4], result[5])
+        elif result[2] == "joke":
+            text = "我來跟你說一個笑話！\n\n[{}]\n\n{}".format(result[4], result[5])
+        
+        # 關閉數據庫的連結
+        cursor.close()
+        connection.close()
+
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
+    except:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))
+
+#=======================================================================================================
+
+def sendText6(event):
+    '''
+    @心情回顧@
+    當使用者點擊@心情回顧@後，
+    會跳出選單讓使用者選擇要回顧的天數，
+    有兩個選擇：今天、一周
+    '''
+    text = event.message.text
+    try:
+        line_bot_api.reply_message(event.reply_token,
+                        TemplateSendMessage(
+                            alt_text='Buttons template',
+                            template=ButtonsTemplate(
+                                title='回顧天數',
+                                text='請選擇要回顧的天數',
+                                actions=[
+                                    MessageTemplateAction(label='_今天_', text='_今天_'),
+                                    MessageTemplateAction(label='_一周_', text='_一周_')
+                                ])))
+    except:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='發生錯誤！\n請聯絡我感恩!'))
+
+#=======================================================================================================
+
+def sendText7(event):
+    '''
+    @心情回顧@
+    當使用者點擊@心情回顧@，可以返回當天的心情天氣
+    連結MYSQL，索要userid、phq9_score、picture_score、diary_score分數加總
+    '''
+    line_userid = event.source.user_id
+    # 資料庫設定
+    db_settings = {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'abc123',
+        'db': 'line_catcher',
+        'charset': 'utf8'
+    }
+    
+        # 建立 connection 物件
+    connection = pymysql.connect(**db_settings)
+        # 製作一個指標，只要執行成功，就會回傳訊息
+    with connection.cursor() as cursor:
+        all_score = (
+                "SELECT phq9.userID,(phq9.Q1+phq9.Q2+phq9.Q3+phq9.Q4+phq9.Q5+phq9.Q6+phq9.Q7+phq9.Q8+phq9.Q9) as phq9_score,\
+                SUM(app_catcher_diary.text_score) / COUNT(current_date()-1) AS text_score,\
+                CAST(app_catcher_diary.created_at AS DATE) AS diary_date,\
+                app_catcher_picture.picture_score AS pictre_score,\
+                CAST(app_catcher_picture.created_at AS DATE) AS picture_date\
+                FROM phq9\
+                LEFT OUTER JOIN app_catcher_diary ON phq9.UserID=app_catcher_diary.diary_uid\
+                LEFT OUTER JOIN app_catcher_picture ON phq9.UserID=app_catcher_picture.picture_uid\
+                where current_date()-1 = CAST(app_catcher_diary.created_at AS DATE)\
+                AND current_date()-1 = CAST(app_catcher_picture.created_at AS DATE)\
+                group by CAST(app_catcher_diary.created_at AS DATE), UserID"                
+            )
+        cursor.execute(all_score)
+    connection.commit()
+        
+    result = cursor.fetchall()
+ 
+    cursor.close()
+    connection.close()
+    print('MySQL connection is closed.')
+    
+    # 將數據tuple轉換為DataFrame
+    col = ["userid", "phq9_score", "text_score", "text_date", "picture_score", "picture_date"]
+    df = pd.DataFrame(list(map(list, result)), columns = col)
+    # 核對使用者ID，將正確的數據抽取
+    index = df[df.userid == line_userid]
+    phq9_oneday = df.phq9_score.loc[0]
+    text_oneday = df.text_score.loc[0]
+    picture_oneday = df.picture_score.loc[0]
+    
+    try:
+        if phq9_oneday < 5:          # 極輕度焦慮
+            if text_oneday > 1.33 and picture_oneday == 1:     # 健康文本，開心臉
+                text = "今天的心情天氣是晴朗艷陽天"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 2:   # 健康文本，中性臉
+                text = "今天的心情天氣是晴朗艷陽天"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 3:   # 健康文本，傷心臉
+                text = "今天的心情天氣是烏雲密布"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 3:  # 憂鬱文本，傷心臉
+                text = "今天的心情天氣是烏雲密佈"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            else:
+                text = "今天的心情天氣是多雲小日子"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+        
+        elif 5 <= phq9_oneday < 10:  # 輕度焦慮
+            if text_oneday > 1.33 and picture_oneday == 1:     # 健康文本，開心臉
+                text = "今天的心情天氣是晴朗艷陽天"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 2:   # 健康文本，中性臉
+                text = "今天的心情天氣是晴朗艷陽天"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 3:   # 健康文本，傷心臉
+                text = "今天的心情天氣是烏雲密布"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 1:  # 憂鬱文本，生氣臉
+                text = "今天的心情天氣是烏雲密佈"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 3:  # 憂鬱文本，傷心臉
+                text = "今天的心情天氣是陰雨綿綿"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            else:
+                text = "今天的心情天氣是多雲小日子"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+
+        else:                        # 中重度焦慮
+            if text_oneday > 1.33 and picture_oneday == 1:     # 健康文本，生氣臉
+                text = "今天的心情天氣是狂風暴雨"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 2:   # 健康文本，中性臉
+                text = "今天的心情天氣是烏雲密佈"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday > 1.33 and picture_oneday == 3:   # 健康文本，傷心臉
+                text = "今天的心情天氣是傾盆大雨"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 1:  # 憂鬱文本，生氣臉
+                text = "今天的心情天氣是狂風暴雨"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 2:  # 憂鬱文本，中性臉
+                text = "今天的心情天氣是烏雲密佈"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            elif text_oneday <= 1.33 and picture_oneday == 3:  # 憂鬱文本，傷心臉
+                text = "今天的心情天氣是傾盆大雨"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+            else:
+                text = "今天的心情天氣是多雲小日子"
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))    
+
+#=======================================================================================================
+
+def sendText8(event):
+    '''
+    _一周_
+    時間不太夠，若之後要修改，請改動SQL的呼叫
+    詳細的可以參考sendText7的部分
+    '''
+    try:
+        text = '你的日記累積不到七天喔~~\n請再多寫幾天的日記再查詢優~~'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = text))
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))
 
 #=======================================================================================================
 
@@ -156,6 +352,21 @@ def cv_test(img):
 
 #=======================================================================================================
 
+def svm_test(text):
+    #建立function，將列表進行斷詞並用空格連接
+    def segmentWord(cont):
+        c = []
+        for i in cont:
+            a = list(jieba.cut(i))
+            b = " ".join(a)
+            c.append(b)
+        return c
+    # 呼叫模型
+    clf = joblib.load('C:/Users/coco/Desktop/tibame_project/proj_catcher/module/nlp_clf.pkl')
+    return clf.predict(segmentWord(text))
+
+#=======================================================================================================
+
 # 將 gspread 的變量初始化
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('linebotphq9-184c1a155eaa.json', scope)
@@ -183,26 +394,26 @@ def UpdateMySQLTable(sql_data, tableName):
         'charset': 'utf8'
     }
     
-    try:
-        # 建立 connection 物件
-        connection = pymysql.connect(**db_settings)
-        # 製作一個指標，只要執行成功，就會回傳訊息
-        with connection.cursor() as cursor:
-            # 添加數據到 MySQL，若PRIMARY KEY存在則忽略該行，若不存在，則添加新使用者的資料
-            sql_insert_statement = "INSERT IGNORE INTO {}(TIMESTAMP, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, UserID) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(tableName)
-            for i in sql_data:
-                cursor.execute(sql_insert_statement, i)              # 將資料的每一個 row 存進 MySQL
-        connection.commit()
-        print("Table {} successfully updated.".format(tableName))    # 儲存變更成功回傳訊息
-    except pymysql.Error as error:
-        connection.rollback()
-        print("Error: {}. Table {} not updated!".format(error, tableName))
+    
+    # 建立 connection 物件
+    connection = pymysql.connect(**db_settings)
+    # 製作一個指標，只要執行成功，就會回傳訊息
+    with connection.cursor() as cursor:
+        # 添加數據到 MySQL，若PRIMARY KEY存在則忽略該行，若不存在，則添加新使用者的資料
+        sql_insert_statement = "INSERT IGNORE INTO {}(TIMESTAMP, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, UserID) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(tableName)
+        for i in sql_data:
+            cursor.execute(sql_insert_statement, i)              # 將資料的每一個 row 存進 MySQL
+    connection.commit()
+    #print("Table {} successfully updated.".format(tableName))    # 儲存變更成功回傳訊息
+    # except pymysql.Error as error:
+    #     connection.rollback()
+    #     print("Error: {}. Table {} not updated!".format(error, tableName))
         
-    finally: 
-        cursor.close()
-        connection.close()
-        print('MySQL connection is closed.')
+    #finally: 
+    cursor.close()
+    connection.close()
+    print('MySQL connection is closed.')
 
 data = GetSpreadsheetData('PHQ9', 0)
 UpdateMySQLTable(data, 'PHQ9')
-#=======================================================================================================
+
